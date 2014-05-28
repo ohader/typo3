f2ab2ea7eb8753bcd8ca830632575257188e2001
Oliver Hader <oliver@typo3.org>
2014-05-28 11:28:04 +0200

    [BUGFIX] Field size of t3ver_label too low
    During workspace move operations sometimes a warning like
    this can be found:
    
      102: These fields are not properly updated in database:
      (t3ver_label) Probably value mismatch with fieldtype.
    
    The reason is, that the t3ver_label field in almost every
    record is limited to 30 characters in the SQL definition.
    Besides that the error code "102" of the mentioned warning
    is invalid and should just be "1" to be recognized by the
    accordant functional tests as well.
    
    Resolves: #59110
    Releases: 6.2
    Change-Id: Ia97a32dbefd1d27e33f186c19c5a6a495dcc194a

--- typo3/sysext/version/Classes/Hook/DataHandlerHook.php
+++ typo3/sysext/version/Classes/Hook/DataHandlerHook.php
@@ -288,7 +288,7 @@ class DataHandlerHook {
 			) {
 				// Create version of record first, if it does not exist
 				if (empty($WSversion['uid'])) {
-					$tcemainObj->versionizeRecord($table, $uid, 'Placeholder version for moving record');
+					$tcemainObj->versionizeRecord($table, $uid, 'MovePointer');
 					$WSversion = \TYPO3\CMS\Backend\Utility\BackendUtility::getWorkspaceVersionOfRecord($tcemainObj->BE_USER->workspace, $table, $uid, 'uid,t3ver_oid');
 					$this->moveRecord_processFields($tcemainObj, $resolvedPid, $table, $uid);
 				// If the record has been versioned before (e.g. cascaded parent-child structure), create only the move-placeholders
@@ -1261,7 +1261,7 @@ class DataHandlerHook {
 				$newVersion_placeholderFieldArray['perms_group'] = $access['perms_group'];
 				$newVersion_placeholderFieldArray['perms_everybody'] = $access['perms_everybody'];
 			}
-			$newVersion_placeholderFieldArray['t3ver_label'] = 'MOVE-TO PLACEHOLDER for #' . $uid;
+			$newVersion_placeholderFieldArray['t3ver_label'] = 'MovePlaceholder #' . $uid;
 			$newVersion_placeholderFieldArray['t3ver_move_id'] = $uid;
 			// Setting placeholder state value for temporary record
 			$newVersion_placeholderFieldArray['t3ver_state'] = 3;
