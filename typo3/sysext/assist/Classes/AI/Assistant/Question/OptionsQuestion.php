dfa5aa7eb7d191ca1cfd1bda60611500fe6bdd3d
Oliver Hader <oliver.hader@typo3.org>
2026-02-24 17:37:30 +0100

    [TASK] Introduce assistant questions
    Change-Id: Ia7bc8b6dc0d313ed3b4cfc3ee5d0f686a589e35d

---
+++ typo3/sysext/assist/Classes/AI/Assistant/Question/OptionsQuestion.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace TYPO3\CMS\Assist\AI\Assistant\Question;
+
+final readonly class OptionsQuestion implements QuestionInterface
+{
+    /**
+     * @param list<string> $options
+     */
+    public function __construct(
+        public string $text,
+        public array $options,
+    ) {
+        $count = count($options);
+        if ($count < 2 || $count > 4) {
+            throw new \InvalidArgumentException(
+                'OptionsQuestion requires between 2 and 4 options, ' . $count . ' given.',
+                1740000001,
+            );
+        }
+    }
+
+    public function getText(): string
+    {
+        return $this->text;
+    }
+
+    public function jsonSerialize(): array
+    {
+        return [
+            'type' => 'options',
+            'text' => $this->text,
+            'options' => $this->options,
+        ];
+    }
+}
