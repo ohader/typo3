6830787d75c3d678c11c549567fc3a56a8cecc5e
Oliver Hader <oliver.hader@typo3.org>
2026-02-27 15:03:58 +0100

    [BUGFIX] Use correct default SQL type
    Change-Id: I49089ad97c99917f77bbf8e91c69d3e7a5a8d608

--- typo3/sysext/assist/ext_tables.sql
+++ typo3/sysext/assist/ext_tables.sql
@@ -5,7 +5,7 @@ CREATE TABLE sys_assist_progress (
 	 initiator varchar(255) DEFAULT '' NOT NULL,
 	 sequence int(11) unsigned DEFAULT '0' NOT NULL,
 	 type varchar(10) DEFAULT '' NOT NULL,
-	 timestamp datetime(3) NOT NULL,
+	 timestamp datetime(3) DEFAULT CURRENT_TIMESTAMP NOT NULL,
 	 payload longblob,
 
 	 PRIMARY KEY (uuid, sequence)
